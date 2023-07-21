97b77796abe53355c1cfe066c593acf3b070b107
137-AnanthaKumar <96809473+137-AnanthaKumar@users.noreply.github.com>
2023-07-21 11:20:24 +0000

    added new files

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+MYJSON = [
+    {
+        "id": "12",
+        "name": "Thanga anantha Kumar",
+        "age": "23",
+    },
+    {
+        "id": "13",
+        "name": "Akila",
+        "age": "23",
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template("home.html", jobs=MYJSON)
 
+@app.route("/api/getdetails")
+def get_Details():
+    return jsonify(MYJSON)
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
